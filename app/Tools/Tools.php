d44f0d96865a49783786dfc0809ca4f899baea5b
cuimingyang <604692404@qq.com>
2019-09-25 16:16:26 +0800

    cnb

--- app/Tools/Tools.php
+++ app/Tools/Tools.php
@@ -41,7 +41,7 @@ class Tools {
             //不存在
             $result = file_get_contents('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxed4af569360034d1&secret=b71f64cd7e450b160dcef5fffa65ef36');
             $re = json_decode($result,1);
-            dd($result);
+            // dd($result);
             $this->redis->set($access_token_key,$re['access_token'],$re['expires_in']);  //加入缓存
             return $re['access_token'];
         }
